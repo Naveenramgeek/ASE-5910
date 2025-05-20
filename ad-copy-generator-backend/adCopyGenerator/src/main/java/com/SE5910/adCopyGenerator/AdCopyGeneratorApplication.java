f9d6826c1af1d754c75ede54e4cbaa1e84063e13
naveenvemula <naveenvemula2487@gmail.com>
2025-05-20 13:45:17 -0500

    Initial commit: Set up Angular frontend and Spring Boot backend

---
+++ ad-copy-generator-backend/adCopyGenerator/src/main/java/com/SE5910/adCopyGenerator/AdCopyGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.SE5910.adCopyGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdCopyGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdCopyGeneratorApplication.class, args);
+	}
+
+}
---
+++ ad-copy-generator-backend/adCopyGenerator/src/test/java/com/SE5910/adCopyGenerator/AdCopyGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SE5910.adCopyGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdCopyGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
